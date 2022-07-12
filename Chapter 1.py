26f7ab2d25ed8b9f1eaa27befb702383f4fa95bb
gafnts <gafnts@gmail.com>
2022-07-12 15:37:20 -0600

    Testing PyCharm shortcuts

--- Chapter 1.py
+++ Chapter 1.py
@@ -1,3 +1,9 @@
-import numpy as pd
+import numpy as np
 
+vector_row = np.array([1, 2, 3])
 
+vector_column = np.array([[1],
+                         [2],
+                         [3]])
+
+print(vector_row)
