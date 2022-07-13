dbb342a2edac591c0de23436907ce3fc3270a2c7
gafnts <gafnts@gmail.com>
2022-07-13 15:15:32 -0600

    Chapter 1

--- Chapter 1.py
+++ Chapter 1.py
@@ -82,44 +82,95 @@ np.mean(matrix, axis = 0) # Column
 np.mean(matrix, axis = 1) # Row
 
 # Reshaping arrays
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9],
+                   [10, 11, 12]])
 
+matrix.reshape(2, 6)
+matrix.reshape(-1, 1)
+matrix.reshape(1, -1)
+matrix.reshape(12)
 
+# Transposing a vector or a matrix
+matrix.T
+np.array([[1, 2, 3, 4, 5, 6]]).T
 
+# Flattening a matrix
+matrix.flatten()
+matrix.reshape(1, -1)
 
+# Finding the rank of a matrix
+np.linalg.matrix_rank(matrix)
 
+# Calculating the determinant
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
 
+np.linalg.det(matrix)
 
+# Getting the diagonal of a matrix
+matrix.diagonal()
+matrix.diagonal(offset = 1)
+matrix.diagonal(offset = -1)
 
+# Calculating the trace of a matrix
+sum(matrix.diagonal())
+matrix.trace()
 
+# Finding eigenvalues or eigenvectors
+matrix = np.array([[1, -1, 3],
+                   [1, 1, 6],
+                   [3, 8, 9]])
 
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+eigenvalues
+eigenvectors
 
+# Calculating dot products
+vector_a = np.array([1, 2, 3])
+vector_b = np.array([4, 5, 6])
 
+np.dot(vector_a, vector_b)
+vector_a @ matrix
 
+# Adding and substracting matrices
+matrix_a = np.array([[1, 1, 1],
+                     [1, 1, 1],
+                     [1, 1, 2]])
 
+matrix_b = np.array([[1, 3, 1],
+                     [1, 3, 1],
+                     [1, 3, 8]])
 
+np.add(matrix_a, matrix_b)
+np.subtract(matrix_a, matrix_b)
 
+matrix_a + matrix_b
+matrix_a - matrix_b
 
+# Multiplying matrices
+np.dot(matrix_a, matrix_b)
+matrix_a @ matrix_b
 
+matrix_a * matrix_b
 
+# Inverting a matrix
+matrix = np.array([[1, 4, 2],
+                   [2, 5, 1],
+                   [2, 5, 3]])
 
+np.linalg.inv(matrix)
+matrix @ np.linalg.inv(matrix)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Generating random values
+np.random.seed(1)
+np.random.random(3)
+np.random.randint(0, 11, 3)
+np.random.normal(0.0, 1.0, 3)
+np.random.logistic(0.0, 2.0, 3)
+np.random.uniform(1.0, 2.0, 3)
 
 
 
